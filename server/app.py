6fd3a58c2abc5ebc42c1869e6ca5038c074a4a52
Joy <kweyuayieko@gmail.com>
2024-07-02 13:13:55 +0300

    Feat:completed the flask restful api lab

--- server/app.py
+++ server/app.py
@@ -1,27 +1,66 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
+
+class Home(Resource):
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the Plant RESTful API",
+        }
+        response = make_response(response_dict, 200)
+        return response
+
+
+api.add_resource(Home, "/")
+
+
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+
+    def post(self):
+        data = request.json
+
+        new_plant = Plant(name=data["name"], image=data["image"], price=data["price"])
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+        response = make_response(response_dict, 201)
+        return response
+
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            response_dict = plant.to_dict()
+            response = make_response(response_dict, 200)
+        else:
+            response = make_response({"error": "Plant not found"}, 404)
+        return response
 
-if __name__ == '__main__':
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False, default='https://example.com/default_image.jpg')
+    price = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)
+
+    def __repr__(self):
+        return f'<Plant {self.name}, price {self.price}>'
